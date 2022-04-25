965c28b69e6c740375aacca83f8c8bd64fca7cc4
Mamo Tesema <mamo.t@cheq.ai>
2022-04-25 14:57:07 +0300

    remove / from url path in config.js

---
+++ test/utils/expressServer.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const {rtiExpressMiddleware} = require('../../lib/middlewares');
+const eventsTypes = require('../../lib/constans/eventsTypes')
+
+
+//function server(options = {}) {
+
+
+    const options = {}
+    const app = express();
+    const rtiMiddleware  = rtiExpressMiddleware({apiKey: process.env.CHEQ_API_KEY, tagHash: process.env.CHEQ_TAG_HASH, ...options });
+
+    app.use(cookieParser())
+
+    app.get('/', (req, res) => {
+        res.send('test')
+    })
+
+    app.get(`/${eventsTypes.PAGE_LOAD}`, rtiMiddleware(eventsTypes.PAGE_LOAD), (req, res) => {
+        res.send('Hello from CHEQ')
+    })
+
+    app.get(`/${eventsTypes.FORM_SUBMISSION}`, rtiMiddleware(eventsTypes.FORM_SUBMISSION), (req, res) => {
+        res.send('Successfully submitted')
+    })
+
+    app.listen(process.env.TEST_SERVER_PORT, () => {
+        process.stdout.write(`test server listening on port ${process.env.TEST_SERVER_PORT}`)
+    })
+//}
+
+
+//module.exports = server()
+
+
