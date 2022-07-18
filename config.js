2e62f0000f6a4491459595a4ea40edd94009fec6
Mamo Tesema <mamo.t@cheq.ai>
2022-07-18 12:48:10 +0300

    add protocol to logget endpint

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	baseApi: 'https://obstaging.cheqzone.com',
 	rtiPath: 'realtime-interception',
-	rtiLoggerEndpoint: 'rtilogger.production.cheq-platform.com',
+	rtiLoggerEndpoint: 'https://rtilogger.production.cheq-platform.com',
 	rtiTimeout: 100000,
 	defaultApiVersion:'v1'
 };
