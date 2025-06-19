8a0f1cb150a255e6b20b4545c42623104c6291e5
Amit Cohen <amit.c@cheq.ai>
2025-06-19 11:21:55 +0300

    addev rti-v4 support

--- config.js
+++ config.js
@@ -1,28 +1,24 @@
-const pJson = require('./package.json');
+const pJson = require("./package.json");
 
-const rtiConfig = {
-	apiEndpoints: {
-		US: 'https://rti-us-east-1.cheqzone.com',
-		EU: 'https://rti-eu-west-1.cheqzone.com',
-		global: 'https://rti-global.cheqzone.com',
-		DEV: 'https://obs.dev.cheqzone.com',
-	},
-	rtiPath: 'realtime-interception',
-	rtiLoggerEndpoint: 'https://rtilogger.production.cheq-platform.com',
-	rtiTimeout: 100,
-	middlewareVersion: pJson.version
+export const rtiConfig = {
+  rtiPath: "realtime-interception",
+  rtiLoggerEndpoint: "https://rtilogger.production.cheq-platform.com",
+  rtiTimeout: 100,
+  middlewareVersion: pJson.version,
 };
 
-const slpConfig = {
-	apiEndpoints: {
-		PROD: 'https://obs.cheqzone.com',
-		DEV: 'https://obs.dev.cheqzone.com',
-	},
-	slpPath: 'user-validation',
-	slpLoggerEndpoint: 'https://slplogger.production.cheq-platform.com',
-	slpTimeout: 100,
-	middlewareVersion: pJson.version
+export const rtiConfigV4 = {
+  rtiPath: "/defend/4.0/traffic",
+  rtiLoggerEndpoint: "https://rtilogger.production.cheq-platform.com",
+  rtiTimeout: 100,
+  middlewareVersion: pJson.version,
+};
+
+export const slpConfig = {
+  slpPath: "user-validation",
+  slpLoggerEndpoint: "https://slplogger.production.cheq-platform.com",
+  slpTimeout: 100,
+  middlewareVersion: pJson.version,
 };
 
 module.exports = { rtiConfig, slpConfig };
-
